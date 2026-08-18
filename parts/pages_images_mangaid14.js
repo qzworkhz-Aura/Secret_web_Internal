a8cfd9374385b88a5010a6cd0479354ced439c37
qzworkhz-Aura <qzworkhz@gmail.com>
2026-08-18 20:00:35 +0700

    add pages mangaid 14

---
+++ parts/pages_images_mangaid14.js
@@ -0,0 +1,169 @@
+export const pages_mangaid14_part1 = [
+    {
+        src:"/images14/mangaid14_opening(1).jpg",
+        dialog: [
+            "'Aku tersedak karena jumlahnya terlalu banyak, satu demi satu, mengalir ke mulutku. Aku tidak mampu menikmatinya'",
+            "'Toilet ini sudah saya modifikasi dengan berbagai cara, toilet ini adalah tempat kerja kedua saya, tetapi saya menghabislan jauh lebih banyak waktu di sana.'"
+        ],
+        meaning: [
+            "Sambil menggerakkan jari-jari untuk masturbasi. Saat aku melebarkan anusku sepenuhnya, Tai yang tebal dan mengkilap keluar.",
+        ]
+    },
+    {
+        src:"/images14/mangaid14_opening(2).jpg",
+        dialog: [
+            "'Hari yang ku tunggu-tunggu akhirnya tiba. Pasokan air terputus hari ini karena adanya perbaikan pipa di lingkungan sekitarr'",
+            "'Artinya, toiletnya tidak bisa disiram. Kotoran dan urine akan menumpuk di dalam kloset'",
+            "'Putri-putriku datang satu per satu di pagi buta. Buang air kecil dan buang air besar, Bagian dalam kloset adalah pesta pora kekotoran.'",
+            "'Dengan bagian tubuh yang terbuka seluruhnya. Mereka bergiliran membuang kotoran. Anak perempuan saya melihatnya tepat di bawah.'",
+            "'Air kencing berceceran, Tai menumpuk'"
+        ],
+        meaning: [
+            "Di dalam kloset sudah menjadi kekacauan total penuh kotoran. Bau amonia membuat mata tidak bisa dibuka.",
+            "Gadis-gadis yang mengerti kondisi mengerikan ini sama sekali tidak mau melihat ke dalam kloset",
+            "Dari bawah, sosok mereka terlihat sedang mengeluarkan tai. Air kencing yang memercik dan tai yang menumpuk hampir mengubur semuanya"
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part1-s1.jpg",
+        dialog: [
+            "'Maaf mengganggu. Wow, jadi kamu tinggal di rumah seperti ini'",
+            "'Temanku yang berambut panjang langsung masuk ke kamar mandi. Dia tampak sedikit terkejut dengan desain era Showa, tetapi dia melangkah  melewati toilet tanpa ragu-ragu'",
+            "'Saya memeriksa arrea selangkangan untuk melihat apakah ada noda kuning, tetapi tampaknya bersih. Hal-hal seperti inilah yang mengungkapkan bagaimana seseorang dibesarkan.'",
+            "'Namun, aroma wanita yang kuat, menyengat, dan pengap itu tidak bisa disembunyikan'"
+        ],
+        meaning: [
+            "2 Teman sekelas datang bermain. Karena sudah diberitahu sebelumnya, mereka disambut dengan baik dan dipersilakan masuk.",
+            "Teman berambut panjang langsung masuk ke dalam toilet dengan cepat.",
+            "Toilet bergaya Showa yang sedikit mencurigakan itu dilangkahi tanpa rasa curiga.",
+            "Di atas kepala, selangkangan cewek muda terbuka lebar.",
+            "Celana dalam putih menutupinya sedikit terselip dan masuk ke celah pantat.",
+            "Celana dalam di selangkangan tidak terkena noda kening, sehingga kelihatan bersih."
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part1-s2.jpg",
+        dialog: [
+            "'Aku merasa terangsang oleh wanita yang baru pertama kali kutemui hari ini, dan aku menatap lekat-lekat pantatnya yang mulus dan terbuka, alat kelamiin wanita, dan anusnya. (pria)'",
+            "'Hah' (Gadis)",
+            "Apakah kamu tidak terbiasa dengan toilet bergaya jepang? Atau mungkin mereka sedang memainkan pinggul mereka, terlihat tidak nyaman, khawatir ingin buang air kecil"
+        ],
+        meaning: [
+            "Sambil menurunkan pinggang dan menggeser celana dalam ke bawah, kaki panjang cewek itu sangat menarik.",
+            "Pantat yang montok dan organ kewanitaan serta anus yang mengerut terlihat jelas",
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part1-s3.jpg",
+        dialog: [
+            "'Oh begitu, makanya dia terlihat tidak nyaman. Apakah ini penyebabnya?' (pria)"
+        ],
+        meaning: [
+            "Dengan bunyi 'picah-picah-picah' yan kotor, diare encer menyenbur keluar. Tai encer menetes dari anus lalu menumpuk di dasar kloset",
+            "Air kencing dan diare yang menyembur bergema di dalam toilet. Saat diare encer masuk ke mulut, rongga mulut langsung perih.",
+            "Tai ini harus dihindari, Ini pertama kali melihatnya, mengerikan. Berbeda dengan anak-anak di rumah, kondisi tubuhnya tidak bisa dipastikan, jadi tidak tahu apa yang akan keluar."
+        ]
+    }
+];
+
+export const pages_mangaid14_part2 = [
+    {
+        src:"/images14/mangaid14_part2-s1.jpg",
+        dialog: [
+            "'Putri kedua saya mulai kuliah tahun ini, jadi dia sering pulang larut malam.' (pria)",
+            "'Hari ini dia punya bokong besar yang dibalut celana dalam oranye.' (pria)",
+            "'Gadis-gadis cantik juga buang air besar' (pria)",
+            "'Saatnya memuntahkan semua yang telah kamu makan.' (pria)"
+        ],
+        meaning: [
+            "Anak kedua yang tahun ini baru amsuk kuliah dan pulangya lebih malam. Hari ini pantatnya yang besar dibungkus celana dalam bewarna oranye.",
+            "Setelah menurunkan celana dalam sampai lutut dan membuka bagian bawah tubuh di bilik yang tertutup, dia menempelkan selangkangannya ke kloset."
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part2-s2.jpg",
+        dialog: [
+            "'Ini adalah aliran urine yang deras dan seperti jet, khas seorang gadis muda' (pria)",
+            "'Air kencing kuning pertama di pagi hari membentuk genangan di dalam kloset'",
+            "'Aliran urine itu tidak berhenti untuk beberapa saat, membuatku bertanya-tanya berapa banyak yang telah menumpuk di dalam.' (pria)",
+            "'Air kencing anak ini tidak selalu bewarna kuning dan tidak bewarna' (pria)",
+            "'Karena rasanya lebih asam daripada milik anak-anak lain. Mungkin itu beberapa vitamin yang diminumnya'"
+        ],
+        meaning: [
+            "'Syoowarrrr--!' Aliran pipis cewek muda itu seperti jet yang deras dan kuat.",
+            "Air kencing kuning pagi hari membentuk genangan di dalam kloset. Derasnya air kencing tidak berhenti-henti.",
+            "Dari bawah toilet yang dimodifikasim air kencing diminum. Dibandingkan dengan pagi hari kemarin, sepertinya lebih banyak mengandung garam.",
+            "Air kencing anak ini selalu kuning dan tidak pernah bening transparan",
+            "Lebih asam daripada yang lain. Meski pahit karena jumlahnya banyak, tetap diminum sampai habis"
+        ]
+    },
+];
+
+export const pages_mangaid14_part3 = [
+    {
+        src:"/images14/mangaid14_part3-s1.jpg",
+        dialog: [
+            "'Saat ia duduk mengangkang di atas toilet, rok pendeknya menjadi semakin terlihat, seolah-olah kainnya dilipat dua. Area selangkangan juga terlihat jelas.' (pria)",
+            "'Pantatnya yang bulat dan montok terbentur dari atas' (pria)"
+        ],
+        meaning: [
+            "Cewek ketiga yang paling muda suka memakai celana dalam putih. Hari ini dia memakai celana dalam putih polos yang paling bersih.",
+            "Celana dalam katun kekanak-kanakan yang tadi dipakai sudah tidak kelihatan. Saat mengangkang di atas kloset, rok pendeknya terbuka dan kain menjadi dobel sehingga bagian selangkangan terlihat jelas",
+            "Pantat yang montok dan suda berkembang sempurna turun dai atas dengan bunyi 'dosun'. Bentuk anus dan warna bibir kemaluan terlihat sangat jelas"
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part3-s2.jpg",
+        dialog: [
+            "'Air kencing rasanya persis seperti apa yang anda masukkan ke dalam mulut sebelum keluar.' (pria)",
+            "'Dia tampak khawatir kakinya akan semakin membesar karena otot, tetapi dia sepertinya tidak berniat untuk berhenti dari kegiatan klubnya' (pria)",
+            "'Momen buang air kecil pertama putri bungsu saya di pagi hari sunggung berharga.' (pria)",
+            "'Anusnya berkedut dan menonjol, sepertinya ia akan buang air besar.' (pria)"
+        ],
+        meaning: [
+            "Dengan membuka lutut yang rapat, air kencing menyembur keluar. Bukan lurus ke bawah, melainkan sedikit menyebar saat terbang",
+            "Air kencing cewek ini yang menyebar dengan bunyi 'pacha-pacha' sekarang mengotori kloset. Ruang sempit dipenuhi bau amonia",
+            "karena latihan pagi dan bangun pagi, tai pagi cewek ketiga ini terasa berat. Anusnya bergetar dan mengembang, sepertinya dia khawatir akan berak."
+        ]
+    },
+    {
+        src:"/images14/mangaid14_part3-s3.jpg",
+        dialog: [
+            "'Dia menunjukkan tanda-tanda sedikit kesulitan bernapas, ketika bagian tengah anus yang bengkak terbelah. Kotoran bewarna coklat mengintip keluar' (pria)",
+            "'Saat aku mencium bau ini, saya yakin mereka akan berubah pikiran.' (pria)",
+            "'Aku mengendap-endap di belakangnya tanpa dia sadari dan mengambil kotoran yang menjuntai itu. Hangat. Suhu ini adalah suhu tubuh ptri ketiga saya' (pria)",
+            "'Aku sudah tidak tahan lagi. Ciumlah bau tai yang telah dikeluarkan.' (pria)"
+        ],
+        meaning: [
+            "Sedikit menunjukkan tanda-tanda mengeden, pusat anus yang mengembang terbuka dan memperlihatkan kepala tai bewarna coklat",
+            "Aku diam-diam meraih dari belakang dan mengambil tai yang sedang turun.",
+            "Meski mual, aku menghirup dalam-dalam bau busuk yang segar dan menjilati berkali-kali dalam keadaan seperti bermimpi.",
+            "Saat dimasukkan ke mulut dan dikunya dengan kuat lalu ditelan, rasanya selalu sama seperti cewe ketiga."
+        ]
+    }
+];
+
+export const pages_mangaid14_part4 = [
+    {
+        src:"/images14/mangaid14_part4-s1.jpg",
+        dialog: [
+            "'Jelas sekali, anak inilah yang memberi saya kenikmatan terlama saat buang air besar.' (pria)"
+        ],
+        meaning: [
+            "Ahh... menerima langsung air kencing yang keluar ddengan mulut lalu meminumnya, berapa kali pun rasanya tetap luar biasa.",
+            "Baik air kencing maupun tai, potongannya sudah mulai memburuk.",
+            "Hari ini kondisinya sepertinya bagus, suara air kencing terdengar menyenangkan. Hari ini juga pasti kencan dengan cowok, komponen birahi terlihat jelas di dalam air kencingnya"
+        ]
+    },
+    {
+        src:"/images14/mangaid14_ending.jpg",
+        dialog: [
+            "'Sungguh menggembirakan bisa melihat vagina wanita yang terangsang dan sedang birahi, yang akan dipeluk oleh seorang pria, dari dekat, dan dimandikan dalam cairan harumnya' (pria)",
+            "'Kurasa dia tidak bisa menahan diri untuk tidak memikirkan seks hari ini' (pria)",
+            "'Aku selalu menjadi anak yang aktif secara seksual dan mesum, Masturbasi di toilet bukanlah hal yang aneh bagiku; terkadang aku melakukannya hingga 10 kali sehari' (gadis)"
+        ],
+        meaning: [
+            "Pasti karena memikirkan seks hari ini sampai tidak tahan. Anak sulung selalu onani sendirian di bilik sebelum kencan, dan aku mengamatinya dari jarak sangat dekat."
+        ]
+    }
+];
