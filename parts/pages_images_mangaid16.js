e4595914e3afbce84632d7b03c625d7d8e7387f6
qzworkhz-Aura <qzworkhz@gmail.com>
2026-08-19 00:38:27 +0700

    updated: new manga

---
+++ parts/pages_images_mangaid16.js
@@ -0,0 +1,209 @@
+export const pages_mangaid16_part1 = [
+    {
+        src:"/images16/mangaid16_part1-s1.jpg",
+        dialog: [
+            "'WC biasa..'",
+            "'Agak kecewa juga'",
+            "'Biasa aja kan?'",
+            "'Di dalamnya juga tidak ada yang aneh'"
+        ],
+        meaning: [
+            "Dua siswi, dimana satu siswi masuk ke dalam bilik toilet, dimana temannya bertanya apaakah toilet ini ada sesuatu",
+            "Siswi didalam mengangkat rok dan menurunkan celana dalamnya"
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part1-s2.jpg",
+        dialog: [
+            "'Ah, pipisnya keluar'",
+            "'Pipis kuning'",
+            "'Aku belum pernah perhatikan warna pipisku sendiri sih'",
+            "'Apa benar yang kayak gini bisa jadi uang?'",
+            "'Kalau teh sih yang kental lebih bagus, tapi ini kan pipis'"
+        ],
+        meaning: [
+            "Siswi itu berjongkok di antara kloset jongkok dan mulai pipis, ia juga memikirkan bagaimana pipisnya bisa berubah menjadi uang.",
+            "Air kencingnya bewarna kuning dan keluar dengan deras. Dia memperhatikan sendiri warna pipisnya sambil jongkok."
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part1-s3.jpg",
+        dialog: [
+            "'Ngghh...'",
+            "'Keluar, Keluar...!'",
+            "'Tadi siang sebenarnya udah pengen, tapi kutahan sampai sekarang.'",
+            "'Fuuuh...'"
+        ],
+        meaning: [
+            "Setelah pipis, dia mulai berak. Tai panjang bewarna gelap keluar dari anus-nya. Dia mengerang pelan karena lega"
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part1-s4.jpg",
+        dialog: [
+            "'Udah keluar kan?'",
+            "'Uuhh kotor!'",
+            "'Hah? Dasar klosetnya bergerak...'",
+            "'Apa ini, kayak belt conveyor... taiku dibawa pergi!'",
+            "'Tombol lush-nya nggak berfungsi. Palsu ini'",
+            "'Ternyata memang bukan toilet biasa.'",
+            "'Tidak nyangka ada mekanisme kayak gini!'"
+        ],
+        meaning: [
+            "Siswi itu melihat ke dalam kloset setelah selesai",
+            "Tai-nya masih ada, tapi tiba-tiba dsar klose bergerak seperti belt conveyor dan membawa tai-nya",
+            "Tombol flush tidak berfungsi(palsu). Dia kaget"
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part1-s5.jpg",
+        dialog: [
+            "'Gimana?'",
+            "'Taiku dibawa pergi'",
+            "'Dibawa pergi itu maksudnya apa?'",
+            "'Wah, beneran keluar uang'",
+            "'keren kan?'",
+            "'Astaga beneran'",
+            "'Jumlahnya beda sama Ryoko nggak?'",
+            "'Eh! jumlahnya gede banget!'",
+            "'Ah, maaf...'",
+            "'Permisi'"
+        ],
+        meaning: [
+            "Siswi berambut pendek keluar dari bilik dan mengambil uang dari mesin di dinding.",
+            "Teman-temannya kaget karena jumlahnya cukup banyak"
+        ]
+    }
+]
+
+export const pages_mangaid16_part2 = [
+    {
+        src:"/images16/mangaid16_part2-s1.jpg",
+        dialog: [
+            "'Aku datang'",
+            "'Kenapa dipanggil?'",
+            "'Kan nggak bisa keluar berkali-kali dalam sehari.'",
+            "'Ah, jadi hari ini juga mau keluar?'",
+            "'Masuk sini'",
+            "'Bilik paling ujung nyambung ke ruangan lain'",
+            "'Beneran, di balik dinding ada ruangan'",
+            "'Lorong Rahasia'",
+            "'Nggak ada kloset, jadi koridor'",
+            "'Eh, ruangan apa ini?'",
+            "'Toilet? Versi satunya lagi...?'",
+            "'Aku kasih tahu rahasia biar dapat banyak uang.'",
+            "'Cuma pakai toilet ini aja.'"
+        ],
+        meaning: [
+            "Siswi berambut pendek datang lagi bersama teman.",
+            "Mereka masuk ke bilik paling dalam dan menemukan lorong rahasia di balik dinding yang mengarah ke ruangan lain berisi beberapa bilik kosong"
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part2-s2.jpg",
+        dialog: [
+            "'Eh, kenapa?'",
+            "'Ada lubang di lantai.'",
+            "'Kayaknya nggak terlalu dalam.'",
+            "'Di dalam lubang gelap total.'",
+            "'Bisa nggak ya?'",
+            "'Bisa dong, Cuma perlu jongkok di atasnya.'",
+            "'Jadi tinggal keluarin di dalam lubang itu?'",
+            "'Hati-hati, jangan sampe kejebak di lubangnya.'",
+            "'Aku coba! Gampang ini.'",
+            "'Huh, keluar nggak ya...'"
+        ],
+        meaning: [
+            "Mereka melihat ada lubang di lantai.",
+            "Siswi berambut pendek ragu, tapi temannya mendorongnya untuk mencoba jongkok di atas lubang itu."
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part2-s3.jpg",
+        dialog: [
+            "'Fuguuuhhh...'",
+            "'Nggguuuuhhh...'"
+        ],
+        meaning: [
+            "Siswi berambut pendek jongkok di atas lubang dan mulai mendorong.",
+            "Anusnya membuka dan tai panjang mulai keluar."
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part2-s4.jpg",
+        dialog: [
+            "'Ngghh ngghh'",
+            "'Kuuuhh... uunnnaaahhh'"
+        ],
+        meaning: [
+            "Siswi berambut pendek sedang jongkok di atas lubang. Tai-nya keluar panjang dan jatuh ke bawah",
+            "Di bawah lubang ada pria yang membuka mulut dan menerima langsung kotorannya.",
+            "Pria di bawah menelan sambil mengeluarkan suara basah."
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part2-s5.jpg",
+        dialog: [
+            "'Lenganyaaa...'",
+            "'Kayaknya keluar banyak banegt.'",
+            "'Cuma berak dan pipis di dalam lubang doang sih.'",
+            "'Hmm! Dengan ini apa bisa ngalahin jumlah uangnya Ryoko ya?'"
+        ],
+        meaning: [
+            "Siswi itu teras mengeluarkan sisa tai dan pipis.",
+            "Dia bilang dalam hati merasa sudah keluar banyak. Setelah selesai dia menunduk sedikit",
+            "sambil merapikan diri, masih belum sadar ada orang di bawah."
+        ]
+    }
+]
+
+export const pages_mangaid16_part3 = [
+    {
+        src:"/images16/mangaid16_part3-s1.jpg",
+        dialog: [
+            "'Udah keluar? Bedanya apa sama toilet biasa?'",
+            "'Aku juga mau coba.'",
+            "'Keluar banyak lho. Aku yang menang ya.'",
+            "'Hah? Ditentuin dari jumlah yang keluar?'",
+            "'Fufufu, penasaran sama hasilnya.'",
+            "'Hmm ada angin masuk dari lubangnya.'",
+            "'Lubangnya gelap banget.'", 
+            "'Seram.'"
+        ],
+        meaning: [
+            "Teman-temannya (termasuk cewek rambut panjang) ikut mencoba.",
+            "Cewek rambut panjang jongkok di atas lubang yang sama dan mulai berak"
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part3-s2.jpg",
+        dialog: [
+            "'Ngghh! Hari ini agak...'",
+            "'Perutku lagi kurang enak.'",
+            "'Gefuh!'",
+            "'Ih, keluarnya berisik banget.'",
+            "'Kepergok dengar nggak ya?'"
+        ],
+        meaning: [
+            "Cewek rambut panjang mengeluarkan tai yang agak berantakan dan encer.",
+            "Pria di bawah langsung kena di muka dan mulut.",
+            "Cewek di atas sedikit panik karena suaranya terlalu kencang."
+        ]
+    },
+    {
+        src:"/images16/mangaid16_part3-s3.jpg",
+        dialog: [
+            "'Salah satu dari dua orang yang baru datang.'",
+            "'Setelah diare, langsung dilanjut pipis.'",
+            "'Diare yang kena di muka bercampur pipis dan masuk ke mulut.'",
+            "'Cocktail tai dan pipis!'",
+            "'Enak banget!'",
+            "'Aaah, enak...'",
+            "'Pipis panas dari selangkangan masih muda.'"
+        ],
+        meaning: [
+            "Cewek berambut panjangan tiba-tiba diare parah sambil jongkok di atas lubang. Pipis dan tai cair menyembur deras ke bawah.",
+            "Pria di bawah membuka mulut lebar-lebar dan langsung menelan campuran diare + pipis itu. Wajahnya basah kuyup dan dia terlihat sangat menikmatinya."
+        ]
+    }
+]
---
+++ parts/pages_images_mangaplain2.js
@@ -0,0 +1,152 @@
+export const pages_mangaplain2_part1 = [
+    {
+        src:"/imagesplain2/mangaplain2s1.jpg",
+        dialog: [
+            "'An-chan juga...?'",
+            "'Yuki juga mau ke toilet ya...?'",
+            "'Gimana nih...'",
+            "'Repot juga...'"
+        ],
+        meaning: [
+            "Dua siswi berambut pendek hitam dan berambut pirang pendek berdiri di depan toilet sekolah.",
+            "Mereka berdua sama-sama kebelet. Setelah masuk, mereka masing-masing masuk ke bilik."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s2.jpg",
+        dialog: [
+            "'Aku pengen berak tapi...'",
+            "'Aku juga mau berak sih...'",
+            "'Kalau kedengaran suaranya malu banget...'",
+            "'Perutku agak aneh nih...'"
+        ],
+        meaning: [
+            "Di dalam bilik, keduanya saling ngobrol soal ingin berak dalam batinnya. Cewek berambut pirang bilang perutnya aneh.",
+            "Lalu dia menunduk sambil mengangkat rok, siap buang air. Dia takut suaranya kedengaran."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s3.jpg",
+        dialog: [
+            "'Padahal sudah lama banget nggak berak...'",
+            "'Gini mah susah rileks...'",
+            "'Mending tunggu dia selesai dulu deh.'"
+        ],
+        meaning: [
+            "Cewek berambut hitam mencoba santai tapi susah. Dia menurunkan celana dalam sambil berdiri.",
+            "Sementara itu cewek berambut pirang sudah duduk di kloset di biliknya.",
+            "Mereka sepakat siapa yang duluan selesai biar tidak saling dengar."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s4.jpg",
+        dialog: [
+            "'Pipis dulu aja deh.'",
+            "'Huuuh... pipis juga dari tadi ditahan, enak banget.'"
+        ],
+        meaning: [
+            "Cewek berambut pirang tidak kuat menahan pipis. Dia mulai pipis dulu dengan deras sambil duduk di kloset.",
+            "Wajahnya lega camput malu karena sudah tidak kuat."
+        ]
+    }
+]
+
+export const pages_mangaplain2_part2 = [
+    {
+        src:"/imagesplain2/mangaplain2s5.jpg",
+        dialog: [
+            "'Yuki lagi pipis... deras banget...'", 
+            "'Kayaknya dari tadi ditahan...'",
+            "'Aku juga... ikut aja deh...'",
+            "'Haaah... enak banget...'"
+        ],
+        meaning: [
+            "Cewek berambut hitam (An-chan) mendengar suara pipis deras dari Yuki.",
+            "Dia ikut mulai pipis sambil duduk di kloset, wajahnya lega karena sudah tidak kuat menahan."
+        ],
+    },
+    {
+        src:"/imagesplain2/mangaplain2s6.jpg",
+        dialog: [
+            "'Suara gede banget...'",
+            "'Eh...!?'",
+            "'Berbeda dari banyanganku, ternyata dia bisa kenceng banget ya...'",
+            "'kayak yang biasa dilakukan ayah...'",
+            "'Kentutnya Yuki barusan...'"
+        ],
+        meaning: [
+            "Cewek berambut pirang(Yuki) tiba-tiba mengeluarkan kentut yang sangat kencang dan panjang.",
+            "An-chan kaget mendengarnya.",
+            "Yuki sendiri terlihat malu tapi juga agak bangga karena suaranya besar."
+        ],
+    },
+    {
+        src:"/imagesplain2/mangaplain2s7.jpg",
+        dialog: [
+            "'Kentut... aku kentut...'",
+            "'Gimana nih... pasti kedengaran...'",
+            "'Kenapa pas saat kayak gini harus kentut kenceng banget sih...'",
+            "'Nanti ketemu An-chan pakai muka yang mana ya...'"
+        ],
+        meaning: [
+            "Yuki mulai berak.",
+            "Dia  mengerang pelan karena lega tapi sekaligus malu banget karena suaranya pasti kedengaran.",
+            "Dia membayangkan malu kalau ketemu An-chan nanti."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s8.jpg",
+        dialog: [
+            "'Yuki ceper selesai dong...'",
+            "'Udah gawat nih.'",
+            "'An-chan kenapa udah selesai pipis tapi belum keluar...'",
+            "'Pengen cepet berak tapi...'"
+        ],
+        meaning: [
+            "Keduanya masih di bi bilik masing-masing.",
+            "An-chan sudah selesai pipis dan menunggu",
+            "sementara Yuki menahan karena perutnya masih keram dan ingin berak tapi malu."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s9.jpg",
+        dialog: [
+            "'An-chan juga mau berak ya...?'",
+            "'Gimana nih... kalo gini terus jam istirahat bakalan habis...'",
+            "'Barusan kentut kenceng banget, Yuki pasti mau berak juga.'",
+            "'Gawat--Gimana nih'",
+            "'Ah... Kayaknya udah nggak kuat'",
+            "'Pasti yang encer, suaranya bakalan parah'",
+            "'Maaf Yuki... suaranya bakalan kotor banget...'",
+            "'Tolong kasih tanda (sedang berbunyi) dong...'"
+        ],
+        meaning: [
+            "An-chan menebak bahwa Yuki juga ingin berak sama seperti dirinya.",
+            "Yuki sudah tidak kuat dan jam istirahat hampir habis.",
+            "Yuki mulai mencoba mendorong, sambil takut suaranya terlalu kencang."
+        ]
+    },
+    {
+        src:"/imagesplain2/mangaplain2s10.jpg",
+        dialog: [
+            "'Malu sih.. tapi ya sudah...'",
+            "'Yuki aja udah kentut, aku juga boleh dong.'",
+            "'Maaf ya-- aku mulai ya'",
+            "'Aah! Keluar... Keluar...!'",
+            "(Suara berak yang sangat keras dan panjang)",
+            "An-chan: kaget berat mendengar volume suaranya"
+        ],
+        meaning: [
+            "Yuki akhirnya menyerah dan mulai berak.",
+            "Suaranya sangat kencang dan basah (bunyi 'beuuuup' keras). An-chan di bilik sebelah kaget setengah mati mendengarnya."
+        ]
+    }
+]
+
+export const pages_mangaplain2_part3 = [
+    {
+        src:"",
+        dialog: ["To Be Continue"],
+        meaning: ["To Be Continue"],
+    }
+]
